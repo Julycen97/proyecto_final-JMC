3fc958f7e920b57ad451bb5a4b22057d33467a41
Julian Centurion <july_cen@hotmail.com.ar>
2025-05-14 13:57:35 -0300

    agregar productos al carrito implementado mediante sessionStorage, falta usar useState para colocar al lado del icono del carrito un numero con la cantidad de productos actuales

--- proyecto_final-JMC/src/components/products/Tarjeta.jsx
+++ proyecto_final-JMC/src/components/products/Tarjeta.jsx
@@ -3,12 +3,46 @@ import { useState } from 'react';
 import PuntajeProducto from './PuntajeProducto';
 import "./../../styles/components/products/Tarjeta.css"
 
-function agregarCarrito({producto}){
+function actualizarCarrito() {
+    let carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
+    let nuevoCarro = [];
+
+    sessionStorage.removeItem("carrito");
+
+    for (let i = 0; i < carrito.length; i++) {
+        let contador = 0;
+        let indice;
+
+        if (nuevoCarro != null) {
+            for (let e = 0; e < nuevoCarro.length; e++) {
+                if (carrito[i].id == nuevoCarro[e].id) {
+                    contador++;
+                    indice = e;
+                }
+            }
+
+            if (contador == 0) {
+                nuevoCarro.push(carrito[i]);
+            }
+            else {
+                nuevoCarro[indice].cantidad += 1;
+            }
+        }
+    }
+
+    sessionStorage.setItem("carrito", JSON.stringify(nuevoCarro));
+}
+
+function agregarCarrito({ producto }) {
     const carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
 
     carrito.push(producto);
+    
+    carrito[carrito.length - 1].cantidad = 1;
 
     sessionStorage.setItem("carrito", JSON.stringify(carrito));
+
+    actualizarCarrito();
 }
 
 function Tarjeta({ props }) {
@@ -22,11 +56,11 @@ function Tarjeta({ props }) {
             <Card.Img variant="top" src={props.image} />
             <Card.Body className='bodyCard'>
                 <Card.Title>{props.title}</Card.Title>
-                <PuntajeProducto rating={props.rating.rate}/>
+                <PuntajeProducto rating={props.rating.rate} />
                 <span>${props.price}</span>
-                <Button variant="info" onClick={() => agregarCarrito({producto: props})}>Agregar al <i className="bi bi-cart4"></i></Button>
+                <Button variant="info" onClick={() => agregarCarrito({ producto: props })}>Agregar al <i className="bi bi-cart4"></i></Button>
                 <Button variant="secondary" onClick={handleShow}>Detalles <i className="bi bi-search"></i></Button>
-                
+
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title className='h5'>{props.title}</Modal.Title>
--- proyecto_final-JMC/src/pages/Carrito.jsx
+++ proyecto_final-JMC/src/pages/Carrito.jsx
@@ -1,8 +1,52 @@
-function Carrito(){
-    return(
-        <div>
-            CARRITO PAPAAA
-        </div>
+import { Table } from "react-bootstrap";
+
+function recuperarCarrito(){
+    return JSON.parse(sessionStorage.getItem("carrito"));
+}
+
+function carritoVacio(){
+    const carroVacio = [{
+    }]
+
+    return carroVacio;
+}
+
+function Carrito() {
+
+    const carrito =  recuperarCarrito() || carritoVacio();
+
+    return (
+        <Table responsive="sm">
+            <thead>
+                <tr>
+                    <th>Cod. Producto</th>
+                    <th>Título</th>
+                    <th>Categoria</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    carrito.map((prod) => {
+                        return (
+                            <tr key={prod.id}>
+                                <td>{prod.id}</td>
+                                <td>{prod.title}</td>
+                                <td>{prod.category}</td>
+                                <td>${prod.price}</td>
+                                <td>{prod.cantidad}</td>
+                                <td>
+                                    <button className="btn btn-success">+</button>
+                                    <button className="btn btn-danger">-</button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </Table>
     )
 }
 
